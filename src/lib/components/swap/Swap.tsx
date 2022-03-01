256306553fad8bcba6d431351ab28c4a3524ddae
Alexander <valverin95@gmail.com>
2022-03-01 23:39:55 +0300

    [swap] контроль состояния через props

--- src/lib/components/swap/Swap.tsx
+++ src/lib/components/swap/Swap.tsx
@@ -1,17 +1,33 @@
-import { Component, createSignal, JSXElement } from 'solid-js';
+import { Component, createEffect, createSignal, JSXElement } from 'solid-js';
 
 type Props = {
     on: JSXElement;
     off: JSXElement;
+    state: 'on' | 'off';
 }
 
+/**
+ *
+ * @example
+ * <Swap
+ *     on={<i class="fa-solid fa-sun text-4xl"/>}
+ *     off={<i class="fa-solid fa-moon text-4xl"/>}
+ * />
+ */
 export const Swap: Component<Props> = (props) => {
     const [state, setState] = createSignal(false);
 
-    return (
-        <label class="swap swap-rotate" onClick={() => setState(!state())}>
+    createEffect(() => {
+        props.state === 'on'
+            ? setState(true)
+            : setState(false);
+    });
 
-            <input type="checkbox" checked/>
+    return (
+        <label class="swap swap-rotate">
+
+            <input type="checkbox" checked={state()}/>
+
             <span class="swap-on">
                 {props.on}
             </span>
